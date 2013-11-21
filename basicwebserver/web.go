c6ff8cbb6af6b0a33a58aa992c06d70c8446b5d4
Truman Boyes <truman@truman.net>
2013-11-21 17:00:13 -0500

    Added another print statement to do html formatting

--- basicwebserver/web.go
+++ basicwebserver/web.go
@@ -6,6 +6,7 @@ import (
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintf(w, "<html><h1>Suspicious</h1><br>")
     fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
 }
 
